4f0d4ed1833cc7b59cca6966b526cd282a7cb5c4
Alex Markowitz <admarko@gmail.com>
2019-12-25 01:15:19 -0500

    move logins to a config file, pt 4

--- user_logins.py
+++ user_logins.py
@@ -1,10 +1,7 @@
 import hashlib
+import json
 
-logins = {
-	"robert": "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f", 
-	"robert_2": "8551667a4e795fc00a174852ea91307e0b01e39789727aa797db4b9876ee0caf",
-	"robert_3": "bbb916176aecee96c293b9af79ebdaeb5f9e2a5805cc0787b0632bf8e6e16d20",
-}
+logins = {}
 
 def is_valid_credentials(username: str, password: str) -> bool:
 	hashed_pw = hashlib.sha256(password.encode()).hexdigest()
@@ -12,6 +9,9 @@ def is_valid_credentials(username: str, password: str) -> bool:
 
 
 if __name__ == "__main__":
+	with open("credentials.json", "r") as creds:
+		logins = json.load(creds)
+
 	print("\nWelcome! Please sign in:")
 	username = str(input("Username: "))
 	password = str(input("Password: "))
