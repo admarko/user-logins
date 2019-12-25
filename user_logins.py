baa5a17c58867914062b8b3672f6cb99f6127ac1
Alex Markowitz <admarko@gmail.com>
2019-12-25 00:56:39 -0500

    add hashing, part3 done

--- user_logins.py
+++ user_logins.py
@@ -1,22 +1,26 @@
-HARDCODED_USERNAME = "robert"
-HARDCODED_PASSWORD = "password123"
+import hashlib
 
 logins = {
-	HARDCODED_USERNAME: HARDCODED_PASSWORD, 
-	"robert_2": "password123_2",
-	"robert_3": "password123_3",
+	"robert": "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f", 
+	"robert_2": "8551667a4e795fc00a174852ea91307e0b01e39789727aa797db4b9876ee0caf",
+	"robert_3": "bbb916176aecee96c293b9af79ebdaeb5f9e2a5805cc0787b0632bf8e6e16d20",
 }
 
-def is_valid_credentials(username: str, password: str):
-	return username in logins and logins[username] == password
-		
+def is_valid_credentials(username: str, password: str) -> bool:
+	return username in logins and logins[username] == password 
+
+def hash_password(password: str) -> str:
+	return hashlib.sha256(password.encode()).hexdigest()
+
 
 if __name__ == "__main__":
 	print("\nWelcome! Please sign in:")
 	username = str(input("Username: "))
 	password = str(input("Password: "))
 
-	if is_valid_credentials(username, password):
+	hashed_pw = hash_password(password)
+
+	if is_valid_credentials(username, hashed_pw):
 		print("\nmy deepest, darkest secret\n")
 	else:
 		print("\nget lost\n")
