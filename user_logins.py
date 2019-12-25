e488f19149e37bef8c762c9b28891b7137184abe
Alex Markowitz <admarko@gmail.com>
2019-12-25 01:21:24 -0500

    pt 4, edits

--- user_logins.py
+++ user_logins.py
@@ -1,17 +1,15 @@
 import hashlib
 import json
 
-logins = {}
+with open("credentials.json", "r") as creds:
+	LOGINS = json.load(creds)
 
 def is_valid_credentials(username: str, password: str) -> bool:
 	hashed_pw = hashlib.sha256(password.encode()).hexdigest()
-	return username in logins and logins[username] == hashed_pw 
+	return username in LOGINS and LOGINS[username] == hashed_pw 
 
 
 if __name__ == "__main__":
-	with open("credentials.json", "r") as creds:
-		logins = json.load(creds)
-
 	print("\nWelcome! Please sign in:")
 	username = str(input("Username: "))
 	password = str(input("Password: "))
