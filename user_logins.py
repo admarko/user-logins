05d8e71916e5b2804679ca8538e878d7ff26adb0
Alex Markowitz <admarko@gmail.com>
2019-12-25 00:41:27 -0500

    step 2, multiple users

--- user_logins.py
+++ user_logins.py
@@ -1,8 +1,14 @@
 HARDCODED_USERNAME = "robert"
 HARDCODED_PASSWORD = "password123"
 
+logins = {
+	HARDCODED_USERNAME: HARDCODED_PASSWORD, 
+	"robert_2": "password123_2",
+	"robert_3": "password123_3",
+}
+
 def is_valid_credentials(username: str, password: str):
-	return username == HARDCODED_USERNAME and password == HARDCODED_PASSWORD
+	return logins[username] == password
 		
 
 if __name__ == "__main__":
