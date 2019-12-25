85851442560c04a59b9237db525a6af6e438d2c6
Alex Markowitz <admarko@gmail.com>
2019-12-25 00:38:16 -0500

    step 1, edits

--- user_logins.py
+++ user_logins.py
@@ -1,16 +1,16 @@
-def get_user_input():
+HARDCODED_USERNAME = "robert"
+HARDCODED_PASSWORD = "password123"
+
+def is_valid_credentials(username: str, password: str):
+	return username == HARDCODED_USERNAME and password == HARDCODED_PASSWORD
+		
+
+if __name__ == "__main__":
 	print("\nWelcome! Please sign in:")
 	username = str(input("Username: "))
 	password = str(input("Password: "))
-	return username, password
 
-def is_valid_credentials(username: str, password: str):
-	if username == "robert" and password == "password123":
+	if is_valid_credentials(username, password):
 		print("\nmy deepest, darkest secret\n")
 	else:
 		print("\nget lost\n")
-
-
-if __name__ == "__main__":
-	username, password = get_user_input()
-	is_valid_credentials(username, password)
