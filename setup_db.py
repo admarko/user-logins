942eec1fb7a7cdb268f03b7ef839e3106c92b770
Alex Markowitz <admarko@gmail.com>
2019-12-27 01:54:02 -0500

    black reformatting

--- setup_db.py
+++ setup_db.py
@@ -5,38 +5,41 @@ from urllib.request import pathname2url
 from settings import DATABASE
 
 
-# TODO: lookup difference between os and pathname2url
-
 def create_table() -> None:
-	conn = sqlite3.connect(DATABASE)		# pass conn in instead
-	c = conn.cursor()
-	c.execute('''CREATE TABLE users
-				 (username VARCHAR, password_hash VARCHAR)''')
+    conn = sqlite3.connect(DATABASE)
+    c = conn.cursor()
+    c.execute("CREATE TABLE users (username VARCHAR, password_hash VARCHAR)")
 
 
 try:
-	db_URI = 'file:{}?mode=rw'.format(pathname2url(DATABASE))
-	conn = sqlite3.connect(db_URI, uri=True)
-	
-	if conn is not None:
-		c = conn.cursor()
-		c.execute('SELECT name from sqlite_master where type= "table"')
-		tables = [table[0] for table in c.fetchall()]
-		print("\nThe database already exists, with the following table{} : ".format("s" if len(tables) > 1 else ""))
+    db_URI = "file:{}?mode=rw".format(pathname2url(DATABASE))
+    conn = sqlite3.connect(db_URI, uri=True)
 
-		for table in tables:
-			c.execute("Select count(*) from {}".format(table))
-			num_rows = c.fetchone()[0]
-			print("-{} ({} rows)".format(table.capitalize(), num_rows))
+    if conn is not None:
+        c = conn.cursor()
+        c.execute('SELECT name from sqlite_master where type= "table"')
+        tables = [table[0] for table in c.fetchall()]
+        print(
+            "\nThe database already exists, with the following table{}: ".format(
+                "s" if len(tables) > 1 else ""
+            )
+        )
 
-		response = str(input("\nWould you like to delete it and create a new one (y/n)? ")).lower()
-		while response != "y" and response != "n":
-			 response = str(input("Please respond with a (y)es or a (n)o: ")).lower()
-		
-		if response == "n":
-			exit()
-		else:
-			os.remove(DATABASE)
-			create_table()
+        for table in tables:
+            c.execute("SELECT COUNT(*) FROM {}".format(table))
+            num_rows = c.fetchone()[0]
+            print("-{} ({} rows)".format(table.capitalize(), num_rows))
+
+        response = str(
+            input("\nWould you like to delete it and create a new one (y/n)? ")
+        ).lower()
+        while response != "y" and response != "n":
+            response = str(input("Please respond with a (y)es or a (n)o: ")).lower()
+
+        if response == "n":
+            exit()
+        else:
+            os.remove(DATABASE)
+            create_table()
 except sqlite3.OperationalError:
-	create_table()
+    create_table()
--- user_logins.py
+++ user_logins.py
@@ -3,19 +3,20 @@ import hashlib
 import json
 
 with open("credentials.json", "r") as creds:
-	LOGINS = json.load(creds)
+    LOGINS = json.load(creds)
+
 
 def is_valid_credentials(username: str, password: str) -> bool:
-	hashed_pw = hashlib.sha256(password.encode()).hexdigest()
-	return username in LOGINS and LOGINS[username] == hashed_pw 
+    hashed_pw = hashlib.sha256(password.encode()).hexdigest()
+    return username in LOGINS and LOGINS[username] == hashed_pw
 
 
 if __name__ == "__main__":
-	print("\nWelcome! Please sign in:")
-	username = str(input("Username: "))
-	password = getpass()
+    print("\nWelcome! Please sign in:")
+    username = str(input("Username: "))
+    password = getpass()
 
-	if is_valid_credentials(username, password):
-		print("\nMy deepest, darkest secret\n")
-	else:
-		print("\nGet lost\n")
+    if is_valid_credentials(username, password):
+        print("\nMy deepest, darkest secret\n")
+    else:
+        print("\nGet lost\n")
